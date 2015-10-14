b66f09db5288e7a68f8c4c5922577096978893b8
Thomas Brown <tbrown2@luc.edu>
2015-10-14 00:45:15 -0500

    Code to calculate endianess of computer

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+   unsigned int i = 1;
+   char *c = (char*)&i;
+   if (*c)
+       printf("Little endian");
+   else
+       printf("Big endian");
+   getchar();
+   return 0;
+}
